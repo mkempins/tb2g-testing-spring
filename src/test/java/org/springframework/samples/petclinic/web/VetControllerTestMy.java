ed2e5902c1da0ef2c0f04dff01808ecd9849a8d0
Maciej Kempinski <maciej.kempinski@igt.com>
2024-02-13 12:43:19 +0100

    MockMVC

--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTestMy.java
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTestMy.java
@@ -9,6 +9,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.model.Vets;
 import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,6 +21,9 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class VetControllerTestMy {
@@ -30,12 +36,15 @@ class VetControllerTestMy {
 
     List<Vet> vetList;
 
+    MockMvc mockMvc;
+
     @BeforeEach
     void setUp() {
         vetList = new ArrayList<>();
         vetList.add(new Vet());
         when(clinicService.findVets()).thenReturn(vetList);
 
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
@@ -57,4 +66,14 @@ class VetControllerTestMy {
         assertEquals(vetList.size(),vets.getVetList().size());
         verify(clinicService,times(1)).findVets();
     }
+
+    @Test
+    void mockMVCControllerTEst() throws Exception {
+
+        mockMvc.perform(get("/vets.html"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("vets"))
+                .andExpect(view().name("vets/vetList"));
+    }
+
 }
